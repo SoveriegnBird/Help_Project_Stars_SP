c64f003fc372961956198b894bff427382ac3208
SoveriegnBird <jamate13t@gmail.com>
2025-03-24 17:38:19 -0600

    init

---
+++ HELL.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var speed = 175
+var volecity = Vector2()
+
+func _physics_process(delta):
+	velocity = Vector2()
+	if Input.is_action_pressed("down"):
+		velocity.y += speed
+	if Input.is_action_pressed("up"):
+		velocity.y -= speed
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+		$animationPlayer.play("Walk_Right")
+	if Input.is_action_pressed("escape"):
+		get_tree().quit()
+	
+	move_and_slide()
